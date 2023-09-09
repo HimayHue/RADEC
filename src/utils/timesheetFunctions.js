bf0987e1a46ef0897f01654b0e5514bbd3470742
HimayRadec <80723658+HimayRadec@users.noreply.github.com>
2023-09-09 08:41:56 -0700

    Create timesheetFunctions.js

---
+++ src/utils/timesheetFunctions.js
@@ -0,0 +1,27 @@
+function clockIn() {
+  return new Date();
+}
+
+function clockOut() {
+  return new Date();
+}  
+
+function calculateHoursWorked(clockIn, clockOut) {
+  return clockOut - clockIn;
+}
+
+function findTimesheet() {
+  return timesheet;
+}
+
+function findMonthTimesheet() {
+  return timesheet;
+}
+
+function findDayTimesheet() {
+  return timesheet;
+}
+
+function updateActiveProject() {
+  return timesheet;
+}
