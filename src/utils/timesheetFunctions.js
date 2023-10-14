9727c776308a273fbeeb0a50df4c8078bdba63bd
HimayRadec <80723658+HimayRadec@users.noreply.github.com>
2023-10-14 12:17:01 -0700

    Update timesheetFunctions.js
    Cleaned up code.

--- src/utils/timesheetFunctions.js
+++ src/utils/timesheetFunctions.js
@@ -24,15 +24,16 @@ let employeesInfo = {};
 
 // Make clockIn and clockOut check if the employee is already clocked in or out
 function clockIn(name, employeeID, clockedInTime) {
-  // Check if you are already clocked in do not clock in again
-  if (!employeesInfo[name]) {
+  const employeeInfoExists = employeesInfo[name];
+
+  if (!employeeInfoExists) {
     employeesInfo[name] = {};
     employeesInfo[name].employeeID = employeeID;
     employeesInfo[name].clockedInTime = clockedInTime;
 
     return clockedInTime;
   }
-  else if (!employeesInfo[name].clockedInTime) { // usually when you clock out the whole object is deleted
+  else if (!employeeInfoExists.clockedInTime) { // usually when you clock out the whole object is deleted
     employeesInfo[name].clockedInTime = clockedInTime;
     return clockedInTime;
   }
@@ -46,14 +47,17 @@ function clockIn(name, employeeID, clockedInTime) {
 function clockOut(employee, clockedOutTime) {
   // Check if the employee is clocked in
   console.log(`ran clockOut for ${employee}`);
-  if (employeesInfo[employee] && employeesInfo[employee].clockedInTime) {
+
+  const employeeIsClockedIn = employeesInfo[employee] && employeesInfo[employee].clockedInTime;
+
+  if (employeeIsClockedIn) {
 
     const hoursWorked = calculateHoursWorked(employeesInfo[employee].clockedInTime, clockedOutTime);
     employeesInfo[employee] = { clockedInTime: null };
 
     return {
       clockedOutTime: clockedOutTime,
-      hoursWorked: hoursWorked.toFixed(3),
+      hoursWorked: hoursWorked,
     };
 
   }
@@ -64,8 +68,8 @@ function clockOut(employee, clockedOutTime) {
 
 function calculateHoursWorked(clockIn, clockOut) {
   const millisecondsWorked = clockOut - clockIn;
-  const hoursWorked = millisecondsWorked / (1000 * 60 * 60); // Convert milliseconds to hours
-  return hoursWorked;
+  const hoursWorked = millisecondsWorked / (1000 * 60 * 60); 
+  return hoursWorked.toFixed(3);
 }
 
 function createYearNewTimesheet(employeeInfo) {
@@ -127,64 +131,40 @@ function createNewSessionTimesheet(timeIn, timeOut) {
  * 
  * ****************/
 
-async function findYearTimesheet(employeeID, year) {
+async function findYearTimesheet(employeeID, year, mustExist = false) {
   try {
-    const timesheet = await Timesheet.findOne({ employeeID: employeeID, year: year });
-
-    if (timesheet) {
-      return timesheet;
-    }
-
-  } catch (error) {
-
-    console.error(`Error while finding Timesheet for year ${year}: ${error.message}`);
-    throw error;
+    let yearTimesheet = await Timesheet.findOne({ employeeID: employeeID, year: year });
+    if (yearTimesheet) return yearTimesheet;
+  } 
+  catch (error) {
+    return null;
   }
-
 }
 
-async function findMonthTimesheet(employeeID, year, monthNumber) {
+async function findMonthTimesheet(employeeID, year, monthNumber, mustExist = false) {
   try {
     let monthTimesheet = (await findYearTimesheet(employeeID, year)).months.find((m) => m.month === monthNumber);
-
-    if (monthTimesheet) {
-      return monthTimesheet;
-    }
-    else { // should not run becase it will throw an error???
-      // console.log(`No Timesheet found for month ${monthNumber}`);
-      return null;
-    }
-
-    // return (monthTimesheet? monthTimesheet : null)
-
+    if (monthTimesheet) return monthTimesheet;
   }
   catch (error) {
-    // console.error(`Error while finding month Timesheet for ${monthNumber}/${year}: ${error.message}`);
-    throw error;
+    return null;
   }
 }
 
 async function findDayTimesheet(employeeID, year, monthNumber, day, mustExist = false) {
-
   try {
-
     let dayTimesheet = (await findMonthTimesheet(employeeID, year, monthNumber)).days.find((d) => d.day == day);
-
-    if (dayTimesheet) {
-      return dayTimesheet;
-    }
-    else { // should not run becase it will throw an error???
-      // console.log(`(findDayTimesheet) No Timesheet found for day ${day}`);
-      return null;
-    }
-
+    if (dayTimesheet) return dayTimesheet;
   }
   catch (error) {
-    // console.error(`Error while finding day Timesheet for ${monthNumber}/${day}/${year}: ${error.message}`);
-    throw error;
+    if (mustExist) {
+      let dayTimesheet = createNewDayTimesheet(day);
+      return dayTimesheet;
+    }
+    else {
+      return null;
+    }
   }
-
-
 }
 
 function updateActiveProject() {
@@ -230,6 +210,8 @@ async function getDayHours(employeeID, year, month, day) {
 }
 
 async function saveShiftToDatabase(dayTimesheet, employeeShiftInfo) {
+
+
 
 }
 
